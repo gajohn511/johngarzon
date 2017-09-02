39dea0837e862cf1af05b9273f122988db0542f4
John Garzon <gajohn511@gmail.com>
2017-09-02 02:36:19 -0400

    Added header, logo, and parallax background

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //  Bootstrap
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
+app.use("/fonts", express.static(__dirname + "/node_modules/bootstrap/fonts")); // bootstrap glyphicons halflings
 
 app.use("/", routes);
 // app.use("/users", users);
